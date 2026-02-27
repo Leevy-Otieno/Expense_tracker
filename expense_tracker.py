73a4bef2f4ab876f2bc9da89e73bbbc3b50e1a10
Leevy-Otieno <leevyotieno@gmail.com>
2026-02-27 14:59:19 +0300

    firstcommit

---
+++ expense_tracker.py
@@ -0,0 +1,28 @@
+
+def main():
+    print("Running the expense tracker application...")
+    # Get user input for expense
+    get_user_expense()
+    
+
+    # write their expense to a file
+    save_expense_to_file(
+        
+    )
+
+
+    # read the file and display the expenses
+    summarize_expenses()
+    
+
+def get_user_expense():
+     print(f'Enter the name of the expense: ')
+
+def save_expense_to_file():
+     print(f'Saving expenses: ')
+
+def summarize_expenses():
+     print(f'Summerising expenses: ')
+     
+if __name__ == "__main__":
+     main()
