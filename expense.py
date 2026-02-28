d100aade36d3002ca1ae015c2fcaa2c9e82bd9a0
Leevy-Otieno <leevyotieno@gmail.com>
2026-02-28 16:10:48 +0300

    second commit

--- expense.py
+++ expense.py
@@ -1,6 +1,12 @@
 class Expense:
-    def __init__(self,name,category, amount)->None:
+<<<<<<< HEAD
+ 
+=======
+    def __init__(self, name, category, amount) -> None:
         self.name = name
         self.category = category
         self.amount = amount
-        
+
+    def __repr__(self):
+        return f"<Expense: {self.name}, {self.category}, ${self.amount:.2f} >"
+>>>>>>> 923ee62 (second Commit)
